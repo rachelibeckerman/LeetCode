efe07d648bdb66f5546c7f94b768c6325f71f2a9
rachelibeckerman <144619300+rachelibeckerman@users.noreply.github.com>
2025-05-11 18:47:10 +0300

    Time: 7 ms (59.25%), Space: 18.7 MB (87.14%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,24 +1,34 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        i = 0
-        while i < len(asteroids):
-            if(asteroids[i] < 0):
-                j = i -1
-                while(j >= 0):
-                    if(asteroids[j] > 0):
-                        if(abs(asteroids[i]) > abs(asteroids[j])):
-                            del asteroids[j]
-                            i -= 1
-                        elif(abs(asteroids[i]) == abs(asteroids[j])):
-                            del asteroids[i]
-                            del asteroids[j]
-                            i -= 2
-                            break
-                        else:
-                            del asteroids[i]
-                            i -= 1
-                            break
-                    j -= 1
-            i += 1
-                    
-        return asteroids
+        # i = 0
+        # while i < len(asteroids):
+        #     if(asteroids[i] < 0):
+        #         j = i -1
+        #         while(j >= 0):
+        #             if(asteroids[j] > 0):
+        #                 if(abs(asteroids[i]) > abs(asteroids[j])):
+        #                     del asteroids[j]
+        #                     i -= 1
+        #                 elif(abs(asteroids[i]) == abs(asteroids[j])):
+        #                     del asteroids[i]
+        #                     del asteroids[j]
+        #                     i -= 2
+        #                     break
+        #                 else:
+        #                     del asteroids[i]
+        #                     i -= 1
+        #                     break
+        #             j -= 1
+        #     i += 1
+        stack = []
+        for a in asteroids:
+            while(stack and a < 0 and stack[-1] > 0 ):
+                    if(stack[-1] < abs(a)):
+                        stack.pop()
+                        continue
+                    elif(stack[-1] == abs(a)):
+                        stack.pop()
+                    break
+            else:
+                stack.append(a)
+        return stack
